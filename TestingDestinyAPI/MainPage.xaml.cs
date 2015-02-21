668be9eaa5af9415ed9f170a6f1cc97b9e9f7a74
steplaugh <qeadzcs2@>
2015-02-20 19:14:05 -0500

    Learning to Parse through the Json Object

--- TestingDestinyAPI/MainPage.xaml.cs
+++ TestingDestinyAPI/MainPage.xaml.cs
@@ -17,11 +17,15 @@ using System.IO.IsolatedStorage;
 using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Newtonsoft.Json.Linq;
+
 
 //http://json2csharp.com/#
 //http://blogs.msdn.com/b/pakistan/archive/2013/06/14/getting-started-with-web-requests-amp-json-in-windows-phone.aspx
 //API directories.
 //http://pastebin.com/GTrVtScf
+//Getting GITHUB to work with VS13
+//http://michaelcrump.net/setting-up-github-to-work-with-visual-studio-2013-step-by-step
 
 namespace TestingDestinyAPI
 {
@@ -117,31 +121,84 @@ namespace TestingDestinyAPI
         //****************************************************************************************
         private void LogIn_btn_Click(object sender, RoutedEventArgs e)
         {
-            //UserID = UserInput_txt.Text;
-            //string url = "http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/" + ConsoleVersion + "/" + UserID +"/";
-            WebClient webClient = new WebClient();
-            string test = string.Format("http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/{0}/{1}/", ConsoleVersion, UserInput_txt.Text);
-            webClient.DownloadStringCompleted += webClient_DownloadStringCompleted;
-            webClient.DownloadStringAsync(new Uri(string.Format("http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/{0}/{1}/", ConsoleVersion, UserInput_txt.Text)));
-            MessageBox.Show(test);
+
+            Download();
+
+           
         }
 
-        private void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+
+        
+        public void Download()
+        {
+            DownloadString((result) =>
+            {
+                
+                //!!Require to import Newtonsoft.Json.dll for JObject!!
+                JObject fdata = JObject.Parse(result);
+
+                //****************
+                // RIGHT HERE !! *
+                //****************
+
+                //I want to be able to parse through the Json object till I find "membershipId" and then display the contents.
+
+                UserInput_txt.Text = fdata["Message"].ToString();
+
+                //********
+                // /END **
+                //********
+
+            }, "http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/1/leroyjenkins980/");
+        }
+
+        public void DownloadString(Action<string> callback, string url)
+        {
+            WebClient client = new WebClient();
+            client.DownloadStringCompleted += (p, q) =>
+            {
+                if (q.Error == null)
+                {
+                    callback(q.Result);
+
+                }
+
+            };
+
+            client.DownloadStringAsync(new Uri(url));
+
+        }
+    
+
+        // **************************************
+        // **  OLD CODE I DON'T USE RIGHT NOW  **
+        // **************************************
+
+        // **** Used to retrieve the info (Doesn't work) ****
+        
+        //UserID = UserInput_txt.Text;
+        //string url = "http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/" + ConsoleVersion + "/" + UserID +"/";
+        //WebClient webClient = new WebClient();
+        //string test = string.Format("http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/{0}/{1}/", ConsoleVersion, UserInput_txt.Text);
+
+        //webClient.DownloadStringCompleted += webClient_DownloadStringCompleted;
+        //webClient.DownloadStringAsync(new Uri(string.Format("http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/{0}/{1}/", ConsoleVersion, UserInput_txt.Text)));
+        //MessageBox.Show(test);
+
+
+        
+        /*private void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
         {
             try
             {
                 if (!string.IsNullOrEmpty(e.Result))
                 {
                     //Parse result into Json string
+                    
                     //var root1 = JsonConvert.DeserializeObject<RootObject>(e.Result);
                     var root2 = JsonConvert.DeserializeObject<Response>(e.Result);
                     this.DataContext = root2;
                     MessageBox.Show(root2.membershipId);
-                    //UserInput_txt.Text = root2.membershipId.ToString();
-
-                    //ImageBrush imb = new ImageBrush();
-                    //imb.ImageSource = new BitmapImage(new Uri(root1.Response.
-                    //StackPanelBackground.Background = imb;
                 }
             }
             catch (Exception ex)
@@ -149,29 +206,9 @@ namespace TestingDestinyAPI
                 MessageBox.Show(ex.ToString());
             }
         }
-        
+        */
 
 
-
-       
-
-
-
-
-
-        /*public class membershipID
-     {
-         public string Response { get; set; }
-         public string iconPath { get; set; }
-         public string membershipType { get; set; }
-         public string membershipId { get; set; }
-         public string displayName { get; set; }
-         public string ErrorCode { get; set; }
-         public string ThrottleSeconds { get; set; }
-         public string ErrorStatus { get; set; }
-         public string Message{ get; set; }
-         public string MessageData { get; set; }
-     }*/
 
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
